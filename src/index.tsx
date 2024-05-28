fb1a56ef69cd0cffb648af28261beb41f1e4d2fe
Ganerdene0087 <66080334+Ganerdene0087@users.noreply.github.com>
2024-05-29 06:13:33 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 
 const client = new ApolloClient({
-  uri: "num-medical-assistance-backend.vercel.app/graphql",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
   credentials: "include",
 });
